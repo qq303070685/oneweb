bee7452db4cb868f3629d703b1e6d1ece194118b
Lzy <31952542+qq303070685@users.noreply.github.com>
2018-06-18 12:40:46 +0800

    修改setting

--- onesite/onesite/settings.py
+++ onesite/onesite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd)qu08d=nd05d^*%2ea+7*2i5j(sw#p8)#cm39le5(os10rl*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
